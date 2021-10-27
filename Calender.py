113ff80758ad21cbf2801b9dcfe4a79f06918d39
donghyun_daniel <donghyun.main@gmail.com>
2021-10-27 19:48:11 +0900

    updated

--- Calender.py
+++ Calender.py
@@ -1,9 +1,10 @@
 import pandas as pd
-start = input("Enter Start Period YYYYMMDD")
-end = input("Enter End Period YYYYMMDD")
+start = input("Enter Start Period YYYYMMDD : ")
+end = input("Enter End Period YYYYMMDD : ")
 mondays = pd.date_range(start=start, end=end, freq='WOM-1SUN')
 ans = []
 for m in mondays:
     if m.day == 1:
         ans.append(m)
+        print(m)
 print(f"There are {len(ans)} Sundays which is first date of each months")
